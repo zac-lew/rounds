a16612b87fa9a97d3c47b649fb324950fc4009d3
Zac <zaclew922@gmail.com>
2019-05-03 14:39:18 +1000

    Basic drink ordering and receiving added

--- src/Customer/Customer.js
+++ src/Customer/Customer.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "reactstrap";
+import "./Customer.css";
 
-const Customer = props => {
-  return (
-    <div className="customer-container">
-      <h1>Customer</h1>
-      <Link to="/">Home</Link>
-    </div>
-  );
-};
+const URL = "ws://192.168.1.12:3030";
+
+class Customer extends Component {
+  state = {};
+  ws = new WebSocket(URL);
+  componentDidMount() {
+    this.ws.onopen = () => {
+      console.log("connected");
+    };
+  }
+  sendOrder = order => {
+    this.ws.send(JSON.stringify(order));
+  };
+  generateButtons = () => {
+    return drinks.map(drink => (
+      <Button color="success" onClick={() => this.sendOrder(drink)} key={drink}>
+        {drink}
+      </Button>
+    ));
+  };
+  render() {
+    return (
+      <div className="customer-container">
+        <h1>Customer</h1>
+        <div className="subtitle">Drinks List:</div>
+        <div className="drinks-list">{this.generateButtons()}</div>
+        <Link to="/">Home</Link>
+      </div>
+    );
+  }
+}
+
+const drinks = ["Espresso Martini", "Margarita", "Dark & Stormy"];
 
 export default Customer;
--- src/Merchant/Merchant.js
+++ src/Merchant/Merchant.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-const Merchant = props => {
-  return (
-    <div className="merchant-container">
-      <h1>Merchant</h1>
-      <Link to="/">Home</Link>
-    </div>
-  );
-};
+const URL = "ws://192.168.1.12:3030";
+
+class Merchant extends Component {
+  state = {
+    orders: []
+  };
+  ws = new WebSocket(URL);
+  componentDidMount() {
+    this.ws.onopen = () => {
+      console.log("connected");
+    };
+    this.ws.onmessage = evt => {
+      const order = JSON.parse(evt.data);
+      this.addOrder(order);
+    };
+  }
+  addOrder = order => {
+    this.setState({ orders: [...this.state.orders, order] });
+  };
+  generateOrders = () => {
+    return this.state.orders.map((order, i) => <h2 key={i}>{order}</h2>);
+  };
+  render() {
+    return (
+      <div className="merchant-container">
+        <h1>Merchant</h1>
+        <h1>Current Orders:</h1>
+        {this.generateOrders()}
+        <Link to="/">Home</Link>
+      </div>
+    );
+  }
+}
 
 export default Merchant;
